da0a5ab50c79c9f19cce7427ca49063bc4c22093
SyedKamranGH <syed.kam.ahmed@gmail.com>
2025-11-12 15:01:50 +0500

    Default example page

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ExamplePage from "./example/page";
+
+const page = () => {
+  return (
+    <div>
+      <ExamplePage />
+    </div>
+  );
+};
+
+export default page;
