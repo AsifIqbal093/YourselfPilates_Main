8dbfeb68f8f4a2552f4d3acf64f050f18dffe180
yashalsaleem1 <yashalsaleem116@gmail.com>
2025-11-18 16:04:49 +0500

    CTA info text responsiveness fixed

--- src/components/shared/CTAInfoText.jsx
+++ src/components/shared/CTAInfoText.jsx
@@ -7,25 +7,25 @@ import Link from "next/link";
 const CTAInfoText = () => {
   return (
     <div className="flex justify-center bg-white px-4 py-10 sm:px-6 lg:px-8">
-      <div className="mt-25 mb-25 flex items-center gap-4">
-        <Link
-          href="/contactos"
-          className="mt-25 mb-25 flex cursor-pointer items-center gap-4"
-        >
-          <div className="relative h-10 w-10">
-            <Image
-              src="/icons/DarkLogo.png"
-              alt="Logo"
-              fill
-              className="object-contain"
-            />
-          </div>
+      <Link
+        href="/contactos"
+        className="flex flex-col items-center gap-4 text-center sm:flex-row sm:items-center sm:text-left"
+      >
+        {/* Logo */}
+        <div className="relative h-12 w-12 sm:h-10 sm:w-10">
+          <Image
+            src="/icons/DarkLogo.png"
+            alt="Logo"
+            fill
+            className="object-contain"
+          />
+        </div>
 
-          <p className="md:text-1xl text-lg font-normal text-[#15467d] underline sm:text-xl">
-            Gostava de agendar uma visita ou entrar em contacto
-          </p>
-        </Link>
-      </div>
+        {/* Text */}
+        <p className="md:text-1xl text-lg font-normal text-[#15467d] underline sm:text-xl">
+          Gostava de agendar uma visita ou entrar em contacto
+        </p>
+      </Link>
     </div>
   );
 };
