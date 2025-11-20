35e5aaf483850af52b6303ac4ddf63ec511ff8de
yashalsaleem1 <yashalsaleem116@gmail.com>
2025-11-20 18:55:42 +0500

    About Hero images responsiveness fixed

--- src/components/sections/Sobre/AboutHero.jsx
+++ src/components/sections/Sobre/AboutHero.jsx
@@ -26,15 +26,29 @@ const ScrollAnimatedImage = ({ children, side }) => {
     offset: ["start 80%", "end 20%"],
   });
 
-  const x = useTransform(
-    scrollYProgress,
-    [0, 1],
-    side === "left" ? [-150, -350] : [150, 350] // moves more as user scrolls
-  );
-  const opacity = useTransform(scrollYProgress, [0, 1], [1, 1]);
+  const [width, setWidth] = React.useState(1200);
+
+  React.useEffect(() => {
+    const update = () => setWidth(window.innerWidth);
+    update();
+    window.addEventListener("resize", update);
+    return () => window.removeEventListener("resize", update);
+  }, []);
+
+  const x = useTransform(scrollYProgress, (v) => {
+    if (width < 768) {
+      return side === "left" ? -40 * v : 40 * v;
+    }
+
+    if (width < 1024) {
+      return side === "left" ? -80 * v : 80 * v;
+    }
+
+    return side === "left" ? -150 - 200 * v : 150 + 200 * v;
+  });
 
   return (
-    <motion.div ref={ref} style={{ x, opacity }}>
+    <motion.div ref={ref} style={{ x }}>
       {children}
     </motion.div>
   );
@@ -43,13 +57,13 @@ const ScrollAnimatedImage = ({ children, side }) => {
 const AboutHero = () => {
   return (
     <section className="relative mt-[100px] w-full overflow-hidden bg-white pt-10 pb-20">
-      {/* Row 1 */}
+      {/* -------------------------------- ROW 1 -------------------------------- */}
       <div className="grid grid-cols-1 gap-10 px-6 md:grid-cols-2 md:px-16">
         {/* LEFT */}
         <ScrollAnimatedImage side="left">
           <OvalWrapper
             side="left"
-            className="h-[230px] w-[1200px] md:h-[280px] lg:h-[280px]"
+            className="about-hero-left1 h-[180px] w-full sm:h-[200px] sm:w-[420px] md:h-[180px] md:w-[450px] md:-translate-x-12 lg:h-[200px] lg:w-[800px] xl:h-[300px] xl:w-[1200px] xl:-translate-x-[300px]"
           >
             <Image
               src="/images/On/1.jpg"
@@ -60,11 +74,12 @@ const AboutHero = () => {
           </OvalWrapper>
         </ScrollAnimatedImage>
 
+        {/* RIGHT */}
         <ScrollAnimatedImage side="right">
           <div className="relative w-full">
             <OvalWrapper
               side="right"
-              className="absolute right-0 h-[280px] w-[1200px] translate-x-[-250px]"
+              className="about-hero-right1 right-0 h-[180px] w-full sm:h-[200px] sm:w-[420px] md:h-[180px] md:w-[250px] md:translate-x-12 lg:h-[200px] lg:w-[800px] lg:-translate-x-[250px] xl:h-[300px] xl:w-[1000px] xl:-translate-x-[250px]"
             >
               <Image
                 src="/images/On/2.png"
@@ -77,11 +92,13 @@ const AboutHero = () => {
         </ScrollAnimatedImage>
       </div>
 
+      {/* -------------------------------- ROW 2 -------------------------------- */}
       <div className="mt-14 grid grid-cols-1 gap-10 px-6 md:grid-cols-2 md:px-16">
-        <ScrollAnimatedImage side="left">
+        {/* LEFT */}
+        <ScrollAnimatedImage ScrollAnimatedImage side="left">
           <OvalWrapper
             side="left"
-            className="h-[230px] w-[900px] md:h-[250px] lg:h-[280px]"
+            className="about-hero-left2 h-[180px] w-full sm:h-[200px] sm:w-[420px] md:h-[180px] md:w-[400px] md:-translate-x-10 lg:h-[200px] lg:w-[600px] xl:h-[300px] xl:w-[1000px] xl:-translate-x-[300px]"
           >
             <Image
               src="/images/On/3.png"
@@ -92,11 +109,12 @@ const AboutHero = () => {
           </OvalWrapper>
         </ScrollAnimatedImage>
 
-        <ScrollAnimatedImage side="right">
+        {/* RIGHT */}
+        <ScrollAnimatedImage ScrollAnimatedImage side="right">
           <div className="relative w-full">
             <OvalWrapper
               side="right"
-              className="absolute right-0 h-[280px] w-[1200px] translate-x-[-400px]"
+              className="about-hero-right2 right-0 h-[180px] w-full sm:h-[200px] sm:w-[420px] lg:h-[200px] lg:w-[800px] lg:-translate-x-[300px] xl:h-[300px] xl:w-[1000px] xl:-translate-x-[270px]"
             >
               <Image
                 fill
@@ -109,11 +127,13 @@ const AboutHero = () => {
         </ScrollAnimatedImage>
       </div>
 
+      {/* -------------------------------- ROW 3 -------------------------------- */}
       <div className="mt-16 grid grid-cols-1 gap-10 px-6 md:grid-cols-2 md:px-16">
-        <ScrollAnimatedImage side="left">
+        {/* LEFT */}
+        <ScrollAnimatedImage ScrollAnimatedImage side="left">
           <OvalWrapper
             side="left"
-            className="h-[230px] w-[1050px] md:h-[280px] lg:h-[300px]"
+            className="about-hero-left3 h-[200px] w-full sm:h-[230px] sm:w-[440px] md:h-[200px] md:w-[300px] md:-translate-x-17 lg:h-[200px] lg:w-[600px] xl:h-[300px] xl:w-[1000px] xl:-translate-x-[300px]"
           >
             <Image
               src="/images/On/5.png"
@@ -122,8 +142,8 @@ const AboutHero = () => {
               className="object-cover"
             />
 
-            <div className="absolute inset-0 flex items-center justify-center bg-gray-500/50 opacity-80">
-              <div className="relative h-40 w-40 md:h-56 md:w-56 lg:h-72 lg:w-72">
+            <div className="absolute inset-0 flex items-center justify-center bg-gray-500/40">
+              <div className="relative h-32 w-32 sm:h-40 sm:w-40 md:h-56 md:w-56 lg:h-72 lg:w-72">
                 <Image
                   src="/images/On/yourself-pilates-header-logo@2x.png"
                   alt="logo"
@@ -135,8 +155,8 @@ const AboutHero = () => {
           </OvalWrapper>
         </ScrollAnimatedImage>
 
-        <div className="flex max-w-lg items-center">
-          <p className="ml-10 text-[19px] font-normal text-[#15467d]">
+        <div div className="flex max-w-lg items-center md:pl-10">
+          <p className="text-center text-[17px] font-normal text-[#15467d] sm:text-[18px] md:text-left md:text-[19px]">
             {ABOUT_INTRO.description}
           </p>
         </div>
